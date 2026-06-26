cec3e85f4c252b622f51f9da3727e688fb202352
automationprojects3-ai <automationprojects3@gmail.com>
2026-06-26 17:40:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from os import environ
 
 class Config:
-    API_ID = int(environ.get("API_ID", "0"))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "38498066"))
+    API_HASH = environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
     BOT_TOKEN = environ.get("BOT_TOKEN", "")
     BOT_SESSION = environ.get("BOT_SESSION", "DoneForwardBot")
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://devms786178_db_user:cEtMdLjmHF5EM2Pf@cluster0.xbqyvnn.mongodb.net/?appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "doneforward")
-    BOT_OWNER_ID = [int(x) for x in environ.get("BOT_OWNER_ID", "0").split()]
+    BOT_OWNER_ID = [int(x) for x in environ.get("BOT_OWNER_ID", "8909902924").split()]
     # Force subscribe channels (Telegram channel IDs or usernames, comma separated)
-    FSUB_CHANNELS = [x.strip() for x in environ.get("FSUB_CHANNELS", "").split(",") if x.strip()]
+    FSUB_CHANNELS = [x.strip() for x in environ.get("FSUB_CHANNELS", "-1003330631655,-1003910364346,-1004361013010").split(",") if x.strip()]
 
 class temp:
     BANNED_USERS = []
